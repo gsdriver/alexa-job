730ddbbb2fa8d37310bb711c0be4282a29ff744d
gsdriver <gsdriver@live.com>
2017-06-13 17:11:37 -0700

    Offline job processor

---
+++ index.js
@@ -0,0 +1,87 @@
+//
+// Utility functions
+//
+
+'use strict';
+
+const roulette = require('./roulette');
+const blackjack = require('./blackjack');
+const slotmachine = require('./slotmachine');
+const AWS = require('aws-sdk');
+AWS.config.update({
+  accessKeyId: process.env.accessKeyId,
+  secretAccessKey: process.env.secretAccessKey,
+  region: 'us-east-1',
+});
+const SES = new AWS.SES();
+
+function sendEmail(text, callback) {
+  const digestName = (new Date(Date.now()).getHours() < 12)
+          ? 'Alexa Skill Usage Morning Digest'
+          : 'Alexa Skill Usage Evening Digest';
+
+  const params = {
+    Destination: {
+      ToAddresses: [
+        process.env.MAILTO,
+      ],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: text,
+          Charset: 'UTF-8',
+        },
+      },
+      Subject: {
+        Data: digestName,
+        Charset: 'UTF-8',
+      },
+    },
+    Source: process.env.MAILFROM,
+  };
+
+  SES.sendEmail(params, callback);
+}
+
+if (process.env.SETS3) {
+  // Get the ranks every 5 minutes and write to S3 if successful
+  setInterval(() => {
+    roulette.updateRouletteScores();
+  }, 1000*60*5);
+}
+
+if (process.env.SENDMAIL) {
+  // Send a summary mail every 12 hours
+  setInterval(() => {
+    // Yes, this is the first run of the day, so let's send an e-mail summary
+    blackjack.getBlackjackMail((bjText) => {
+      slotmachine.getSlotsMail((slotText) => {
+        roulette.getRouletteMail((rouletteText) => {
+          const mailBody = 'BLACKJACK\r\n' + bjText + '\r\n\r\nROULETTE\r\n' + rouletteText + '\r\n\r\nSLOTS\r\n' + slotText;
+
+          console.log(mailBody);
+          sendEmail(mailBody, (err, data) => {
+            if (err) {
+              console.log('Error sending mail ' + err);
+            } else {
+              console.log('Mail sent!');
+            }
+          });
+        });
+      });
+    });
+  }, 1000*60*60*12);
+}
+
+if (process.env.SINGLERUN) {
+  blackjack.getBlackjackMail((bjText) => {
+    slotmachine.getSlotsMail((slotText) => {
+      roulette.getRouletteMail((rouletteText) => {
+        const mailBody = 'BLACKJACK\r\n' + bjText + '\r\n\r\nROULETTE\r\n' + rouletteText + '\r\n\r\nSLOTS\r\n' + slotText;
+
+        console.log(mailBody);
+      });
+    });
+  });
+}
