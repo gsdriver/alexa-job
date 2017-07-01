8e0c129d8005644b1246f58745d3f9275804f19e
gsdriver <gsdriver@live.com>
2017-07-01 05:37:19 -0700

    roulette.js

--- roulette.js
+++ roulette.js
@@ -22,119 +22,125 @@ module.exports = {
     let oldFormat = 0;
     let newFormat = 0;
 
-    // Loop thru to read in all items from the DB
-    (function loop(firstRun, startKey) {
-      const params = {TableName: 'RouletteWheel'};
+    getLastCloseTime((tournamentClose) => {
+      // Loop thru to read in all items from the DB
+      (function loop(firstRun, startKey) {
+        const params = {TableName: 'RouletteWheel'};
 
-      if (firstRun || startKey) {
-        params.ExclusiveStartKey = startKey;
+        if (firstRun || startKey) {
+          params.ExclusiveStartKey = startKey;
 
-        const scanPromise = dynamodb.scan(params).promise();
-        return scanPromise.then((data) => {
-          // OK, let's see where you rank among American and European players
-          let i;
+          const scanPromise = dynamodb.scan(params).promise();
+          return scanPromise.then((data) => {
+            // OK, let's see where you rank among American and European players
+            let i;
 
-          utils.getAdSummary(data, adsPlayed);
-          for (i = 0; i < data.Items.length; i++) {
-             if (data.Items[i].mapAttr && data.Items[i].mapAttr.M) {
-               if (data.Items[i].mapAttr.M.highScore
-                    && data.Items[i].mapAttr.M.highScore.M) {
-                 // This is the old format
-                 oldFormat++;
+            utils.getAdSummary(data, adsPlayed);
+            for (i = 0; i < data.Items.length; i++) {
+               if (data.Items[i].mapAttr && data.Items[i].mapAttr.M) {
+                 if (data.Items[i].mapAttr.M.highScore
+                      && data.Items[i].mapAttr.M.highScore.M) {
+                   // This is the old format
+                   oldFormat++;
 
-                 // Only counts if they spinned
-                 const score = data.Items[i].mapAttr.M.highScore.M;
-                 if (score.spinsAmerican && score.spinsAmerican.N) {
-                   spins = parseInt(score.spinsAmerican.N);
-                   american.spins += spins;
-                   if (spins) {
-                     american.players++;
-                   }
-                   if (parseInt(score.highAmerican.N) > american.high) {
-                     american.high = parseInt(score.highAmerican.N);
-                   }
-                 }
-
-                 if (score.spinsEuropean && score.spinsEuropean.N) {
-                   spins = parseInt(score.spinsEuropean.N);
-                   european.spins += spins;
-                   if (spins) {
-                     european.players++;
-                   }
-                   if (parseInt(score.highEuropean.N) > european.high) {
-                     european.high = parseInt(score.highEuropean.N);
-                   }
-                 }
-               } else {
-                 let scoreData;
-                 newFormat++;
-
-                 if (data.Items[i].mapAttr.M.american && data.Items[i].mapAttr.M.american.M) {
-                   // This is the new format
-                   scoreData = data.Items[i].mapAttr.M.american.M;
-
-                   if (scoreData.spins && scoreData.spins.N) {
-                     spins = parseInt(scoreData.spins.N);
+                   // Only counts if they spinned
+                   const score = data.Items[i].mapAttr.M.highScore.M;
+                   if (score.spinsAmerican && score.spinsAmerican.N) {
+                     spins = parseInt(score.spinsAmerican.N);
                      american.spins += spins;
                      if (spins) {
                        american.players++;
                      }
-                     if (parseInt(scoreData.high.N) > american.high) {
-                       american.high = parseInt(scoreData.high.N);
+                     if (parseInt(score.highAmerican.N) > american.high) {
+                       american.high = parseInt(score.highAmerican.N);
                      }
                    }
-                 }
 
-                 if (data.Items[i].mapAttr.M.european && data.Items[i].mapAttr.M.european.M) {
-                   // This is the new format
-                   scoreData = data.Items[i].mapAttr.M.european.M;
-
-                   if (scoreData.spins && scoreData.spins.N) {
-                     spins = parseInt(scoreData.spins.N);
+                   if (score.spinsEuropean && score.spinsEuropean.N) {
+                     spins = parseInt(score.spinsEuropean.N);
                      european.spins += spins;
                      if (spins) {
                        european.players++;
                      }
-                     if (parseInt(scoreData.high.N) > european.high) {
-                       european.high = parseInt(scoreData.high.N);
+                     if (parseInt(score.highEuropean.N) > european.high) {
+                       european.high = parseInt(score.highEuropean.N);
                      }
                    }
-                 }
+                 } else {
+                   let scoreData;
+                   newFormat++;
 
-                 if (data.Items[i].mapAttr.M.tournament && data.Items[i].mapAttr.M.tournament.M) {
-                   // This is the new format
-                   scoreData = data.Items[i].mapAttr.M.tournament.M;
+                   if (data.Items[i].mapAttr.M.american && data.Items[i].mapAttr.M.american.M) {
+                     // This is the new format
+                     scoreData = data.Items[i].mapAttr.M.american.M;
 
-                   if (scoreData.spins && scoreData.spins.N) {
-                     spins = parseInt(scoreData.spins.N);
-                     tournament.spins += spins;
-                     if (spins) {
-                       tournament.players++;
+                     if (scoreData.spins && scoreData.spins.N) {
+                       spins = parseInt(scoreData.spins.N);
+                       american.spins += spins;
+                       if (spins) {
+                         american.players++;
+                       }
+                       if (parseInt(scoreData.high.N) > american.high) {
+                         american.high = parseInt(scoreData.high.N);
+                       }
                      }
-                     if (parseInt(scoreData.high.N) > tournament.high) {
-                       tournament.high = parseInt(scoreData.high.N);
+                   }
+
+                   if (data.Items[i].mapAttr.M.european && data.Items[i].mapAttr.M.european.M) {
+                     // This is the new format
+                     scoreData = data.Items[i].mapAttr.M.european.M;
+
+                     if (scoreData.spins && scoreData.spins.N) {
+                       spins = parseInt(scoreData.spins.N);
+                       european.spins += spins;
+                       if (spins) {
+                         european.players++;
+                       }
+                       if (parseInt(scoreData.high.N) > european.high) {
+                         european.high = parseInt(scoreData.high.N);
+                       }
+                     }
+                   }
+
+                   if (data.Items[i].mapAttr.M.tournament && data.Items[i].mapAttr.M.tournament.M) {
+                     // This is the new format
+                     scoreData = data.Items[i].mapAttr.M.tournament.M;
+
+                     // Skip this if the tournament closed since they played
+                     if (scoreData.timestamp && scoreData.timestamp.N &&
+                          (scoreData.timestamp.N > tournamentClose)) {
+                       if (scoreData.spins && scoreData.spins.N) {
+                         spins = parseInt(scoreData.spins.N);
+                         tournament.spins += spins;
+                         if (spins) {
+                           tournament.players++;
+                         }
+                         if (parseInt(scoreData.bankroll.N) > tournament.high) {
+                           tournament.high = parseInt(scoreData.bankroll.N);
+                         }
+                       }
                      }
                    }
                  }
                }
              }
-           }
 
-         if (data.LastEvaluatedKey) {
-           return loop(false, data.LastEvaluatedKey);
-         }
-       });
-     }
-    })(true, null).then(() => {
-      text = ('You have ' + american.players + ' people who have done ' + american.spins + ' total spins on an American wheel with a high score of ' + american.high + ' units.\r\n');
-      text += ('You have ' + european.players + ' people who have done ' + european.spins + ' total spins on a European wheel with a high score of ' + european.high + ' units.\r\n');
-      text += ('You have ' + tournament.players + ' people who have done ' + tournament.spins + ' total spins in the tournament with a high score of ' + tournament.high + ' units.\r\n');
-      text += ('There are ' + newFormat + ' people on new-style attributes and ' + oldFormat + ' people with old-style attributes.\r\n');
-      text += utils.getAdText(adsPlayed);
-      callback(text);
-    }).catch((err) => {
-      text = 'Error getting Roulette results: ' + err;
-      callback(text);
+           if (data.LastEvaluatedKey) {
+             return loop(false, data.LastEvaluatedKey);
+           }
+         });
+       }
+      })(true, null).then(() => {
+        text = ('You have ' + american.players + ' people who have done ' + american.spins + ' total spins on an American wheel with a high score of ' + american.high + ' units.\r\n');
+        text += ('You have ' + european.players + ' people who have done ' + european.spins + ' total spins on a European wheel with a high score of ' + european.high + ' units.\r\n');
+        text += ('You have ' + tournament.players + ' people who have done ' + tournament.spins + ' total spins in the tournament with a high score of ' + tournament.high + ' units.\r\n');
+        text += ('There are ' + newFormat + ' people on new-style attributes and ' + oldFormat + ' people with old-style attributes.\r\n');
+        text += utils.getAdText(adsPlayed);
+        callback(text);
+      }).catch((err) => {
+        text = 'Error getting Roulette results: ' + err;
+        callback(text);
+      });
     });
   },
   updateRouletteScores: function() {
@@ -203,99 +209,105 @@ function getRankFromDB(callback) {
   const tournamentScores = [];
   let scoreData;
 
-  // Loop thru to read in all items from the DB
-  (function loop(firstRun, startKey) {
-    const params = {TableName: 'RouletteWheel'};
+  // First find the last tournament close time
+  getLastCloseTime((tournamentClose) => {
+    // Loop thru to read in all items from the DB
+    (function loop(firstRun, startKey) {
+      const params = {TableName: 'RouletteWheel'};
 
-    if (firstRun || startKey) {
-      params.ExclusiveStartKey = startKey;
+      if (firstRun || startKey) {
+        params.ExclusiveStartKey = startKey;
 
-      const scanPromise = dynamodb.scan(params).promise();
-      return scanPromise.then((data) => {
-        // OK, let's see where you rank among American and European players
-        let i;
+        const scanPromise = dynamodb.scan(params).promise();
+        return scanPromise.then((data) => {
+          // OK, let's see where you rank among American and European players
+          let i;
 
-        for (i = 0; i < data.Items.length; i++) {
-          if (data.Items[i].mapAttr && data.Items[i].mapAttr.M) {
-            if (data.Items[i].mapAttr.M.highScore
-                  && data.Items[i].mapAttr.M.highScore.M) {
-              // This is the old-style format
-              // Only counts if they spinned
-              const score = data.Items[i].mapAttr.M.highScore.M;
-              const spinsAmerican = (score.spinsAmerican && score.spinsAmerican.N)
-                    ? parseInt(score.spinsAmerican.N) : 0;
-              const spinsEuropean = (score.spinsEuropean && score.spinsEuropean.N)
-                    ? parseInt(score.spinsEuropean.N) : 0;
-              const highAmerican = (score.highAmerican && score.highAmerican.N)
-                    ? parseInt(score.highAmerican.N) : 0;
-              const highEuropean = (score.highEuropean && score.highEuropean.N)
-                ? parseInt(score.highEuropean.N) : 0;
+          for (i = 0; i < data.Items.length; i++) {
+            if (data.Items[i].mapAttr && data.Items[i].mapAttr.M) {
+              if (data.Items[i].mapAttr.M.highScore
+                    && data.Items[i].mapAttr.M.highScore.M) {
+                // This is the old-style format
+                // Only counts if they spinned
+                const score = data.Items[i].mapAttr.M.highScore.M;
+                const spinsAmerican = (score.spinsAmerican && score.spinsAmerican.N)
+                      ? parseInt(score.spinsAmerican.N) : 0;
+                const spinsEuropean = (score.spinsEuropean && score.spinsEuropean.N)
+                      ? parseInt(score.spinsEuropean.N) : 0;
+                const highAmerican = (score.highAmerican && score.highAmerican.N)
+                      ? parseInt(score.highAmerican.N) : 0;
+                const highEuropean = (score.highEuropean && score.highEuropean.N)
+                  ? parseInt(score.highEuropean.N) : 0;
 
-               if (spinsAmerican) {
-                 americanScores.push(highAmerican);
-               }
-               if (spinsEuropean) {
-                 europeanScores.push(highEuropean);
-               }
-            }
-
-            // Check for new format
-            if (data.Items[i].mapAttr.M.american && data.Items[i].mapAttr.M.american.M) {
-              // This is the new format
-              scoreData = data.Items[i].mapAttr.M.american.M;
-              const spins = (scoreData.spins && scoreData.spins.N)
-                    ? parseInt(scoreData.spins.N) : 0;
-              const high = (scoreData.high && scoreData.high.N)
-                    ? parseInt(scoreData.high.N) : 0;
-
-              if (spins) {
-                americanScores.push(high);
+                 if (spinsAmerican) {
+                   americanScores.push(highAmerican);
+                 }
+                 if (spinsEuropean) {
+                   europeanScores.push(highEuropean);
+                 }
               }
-            }
 
-            if (data.Items[i].mapAttr.M.european && data.Items[i].mapAttr.M.european.M) {
-              // This is the new format
-              scoreData = data.Items[i].mapAttr.M.european.M;
-              const spins = (scoreData.spins && scoreData.spins.N)
-                    ? parseInt(scoreData.spins.N) : 0;
-              const high = (scoreData.high && scoreData.high.N)
-                    ? parseInt(scoreData.high.N) : 0;
+              // Check for new format
+              if (data.Items[i].mapAttr.M.american && data.Items[i].mapAttr.M.american.M) {
+                // This is the new format
+                scoreData = data.Items[i].mapAttr.M.american.M;
+                const spins = (scoreData.spins && scoreData.spins.N)
+                      ? parseInt(scoreData.spins.N) : 0;
+                const high = (scoreData.high && scoreData.high.N)
+                      ? parseInt(scoreData.high.N) : 0;
 
-              if (spins) {
-                europeanScores.push(high);
+                if (spins) {
+                  americanScores.push(high);
+                }
               }
-            }
 
-            if (data.Items[i].mapAttr.M.tournament && data.Items[i].mapAttr.M.tournament.M) {
-              // This is the new format
-              scoreData = data.Items[i].mapAttr.M.tournament.M;
-              const spins = (scoreData.spins && scoreData.spins.N)
-                    ? parseInt(scoreData.spins.N) : 0;
-              const high = (scoreData.bankroll && scoreData.bankroll.N)
-                    ? parseInt(scoreData.bankroll.N) : 0;
+              if (data.Items[i].mapAttr.M.european && data.Items[i].mapAttr.M.european.M) {
+                // This is the new format
+                scoreData = data.Items[i].mapAttr.M.european.M;
+                const spins = (scoreData.spins && scoreData.spins.N)
+                      ? parseInt(scoreData.spins.N) : 0;
+                const high = (scoreData.high && scoreData.high.N)
+                      ? parseInt(scoreData.high.N) : 0;
 
-              if (spins) {
-                tournamentScores.push(high);
+                if (spins) {
+                  europeanScores.push(high);
+                }
+              }
+
+              if (data.Items[i].mapAttr.M.tournament && data.Items[i].mapAttr.M.tournament.M) {
+                // This is the new format
+                scoreData = data.Items[i].mapAttr.M.tournament.M;
+
+                // Only count tournament scores that are still active
+                if (scoreData.timestamp && scoreData.timestamp.N &&
+                    (scoreData.timestamp.N > tournamentClose)) {
+                  const spins = (scoreData.spins && scoreData.spins.N)
+                        ? parseInt(scoreData.spins.N) : 0;
+                  const high = (scoreData.bankroll && scoreData.bankroll.N)
+                        ? parseInt(scoreData.bankroll.N) : 0;
+
+                  if (spins) {
+                    tournamentScores.push(high);
+                  }
+                }
               }
             }
           }
-        }
 
-        if (data.LastEvaluatedKey) {
-          return loop(false, data.LastEvaluatedKey);
-        }
-      });
-    }
-  })(true, null).then(() => {
-    americanScores.sort((a, b) => (b-a));
-    europeanScores.sort((a, b) => (b-a));
-    if (tournamentScores) {
+          if (data.LastEvaluatedKey) {
+            return loop(false, data.LastEvaluatedKey);
+          }
+        });
+      }
+    })(true, null).then(() => {
+      americanScores.sort((a, b) => (b-a));
+      europeanScores.sort((a, b) => (b-a));
       tournamentScores.sort((a, b) => (b-a));
-    }
-    callback(null, americanScores, europeanScores, tournamentScores);
-  }).catch((err) => {
-    console.log('Error scanning: ' + err);
-    callback(err, null, null, null);
+      callback(null, americanScores, europeanScores, tournamentScores);
+    }).catch((err) => {
+      console.log('Error scanning: ' + err);
+      callback(err, null, null, null);
+    });
   });
 }
 
@@ -350,3 +362,17 @@ function checkScoreChange(newScores, callback) {
   });
 }
 
+function getLastCloseTime(callback) {
+  s3.getObject({Bucket: 'garrett-alexa-usage', Key: 'RouletteTournamentResults.txt'}, (err, data) => {
+    if (err) {
+      console.log(err, err.stack);
+      callback(0);
+    } else {
+      // Yeah, I can do a binary search (this is sorted), but straight search for now
+      const results = JSON.parse(data.Body.toString('ascii'));
+
+      results.sort((a, b) => (b.timestamp - a.timestamp));
+      callback(results[0].timestamp);
+    }
+  });
+}
