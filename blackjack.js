0b7b7a10a4906e987bc9cd2e02776829fa510d07
Garrett Vargas <gsdriver@live.com>
2017-07-23 23:01:42 -0700

    Add function to close blackjack tournament

--- blackjack.js
+++ blackjack.js
@@ -89,7 +89,7 @@ module.exports = {
           text += ('There have been ' + progressiveHands + ' hands played towards the progressive. The jackpot has been hit ' + jackpots + ' times.\r\n');
           text += multiplePlays + ' people have played more than one round. ' + maxRounds + ' is the most rounds played by one person.\r\n';
           text += ('Since moving off the service, there have been ' + standardHands + ' hands played. The high score is $' + standardHigh + '.\r\n');
-          text += (tournamentRecent + ' people have played the tournament in the past 24 hours, with ' + tournamentHands + ' played and a high score of $' + tournamentHigh + '.\r\n');
+          text += (tournamentRecent + ' people have played the tournament in the past 24 hours, with ' + tournamentHands + ' hands played and a high score of $' + tournamentHigh + '.\r\n');
           text += utils.getAdText(newads);
           callback(text);
         });
@@ -135,6 +135,42 @@ module.exports = {
       }
     });
   },
+  closeTournament: function(callback) {
+    getEntriesFromDB((err, results, newads) => {
+      if (err) {
+        callback(err);
+      } else {
+        // What's the high score?
+        let highScore = 1;
+        let i;
+
+        for (i = 0; i < results.length; i++) {
+          if (results[i].tournament && (results[i].tournament.bankroll > highScore)) {
+            highScore = results[i].tournament.bankroll;
+          }
+        }
+
+        // Now get the list of completed tournaments to add to
+        s3.getObject({Bucket: 'garrett-alexa-usage', Key: 'BlackjackTournamentResults.txt'}, (err, data) => {
+          if (err) {
+            callback(err);
+          } else {
+            const results = JSON.parse(data.Body.toString('ascii'));
+
+            results.push({timestamp: Date.now(), highScore: highScore});
+            results.sort((a, b) => (a.timestamp - b.timestamp));
+            const params = {Body: JSON.stringify(results),
+              Bucket: 'garrett-alexa-usage',
+              Key: 'BlackjackTournamentResults.txt'};
+
+            s3.putObject(params, (err, data) => {
+              callback(err);
+            });
+          }
+        });
+      }
+    });
+  },
 };
 
 // Function to get all the entries from the Database
@@ -142,78 +178,82 @@ function getEntriesFromDB(callback) {
   const results = [];
   const newads = [];
 
-  // Loop thru to read in all items from the DB
-  (function loop(firstRun, startKey) {
-   const params = {TableName: 'PlayBlackjack'};
+  getLastCloseTime((tournamentClose) => {
+    // Loop thru to read in all items from the DB
+    (function loop(firstRun, startKey) {
+     const params = {TableName: 'PlayBlackjack'};
 
-   if (firstRun || startKey) {
-     params.ExclusiveStartKey = startKey;
+     if (firstRun || startKey) {
+       params.ExclusiveStartKey = startKey;
 
-     const scanPromise = dynamodb.scan(params).promise();
-     return scanPromise.then((data) => {
-       let i;
+       const scanPromise = dynamodb.scan(params).promise();
+       return scanPromise.then((data) => {
+         let i;
 
-       utils.getAdSummary(data, newads);
-       for (i = 0; i < data.Items.length; i++) {
-         if (data.Items[i].mapAttr && data.Items[i].mapAttr.M) {
-           const entry = {};
+         utils.getAdSummary(data, newads);
+         for (i = 0; i < data.Items.length; i++) {
+           if (data.Items[i].mapAttr && data.Items[i].mapAttr.M) {
+             const entry = {};
 
-           entry.numRounds = parseInt(data.Items[i].mapAttr.M.numRounds.N);
-           entry.locale = data.Items[i].mapAttr.M.playerLocale.S;
-           entry.adplayed = (data.Items[i].mapAttr.M.adStamp != undefined);
-           if (data.Items[i].mapAttr.M.standard && data.Items[i].mapAttr.M.standard.M) {
-             const standardGame = data.Items[i].mapAttr.M.standard.M;
+             entry.numRounds = parseInt(data.Items[i].mapAttr.M.numRounds.N);
+             entry.locale = data.Items[i].mapAttr.M.playerLocale.S;
+             entry.adplayed = (data.Items[i].mapAttr.M.adStamp != undefined);
+             if (data.Items[i].mapAttr.M.standard && data.Items[i].mapAttr.M.standard.M) {
+               const standardGame = data.Items[i].mapAttr.M.standard.M;
 
-             entry.standard = {};
-             entry.nonService = true;
-             if (standardGame.hands && standardGame.hands.N) {
-               entry.standard.hands = parseInt(standardGame.hands.N);
-             }
-             if (standardGame.timestamp && standardGame.timestamp.N) {
-               entry.standard.timestamp = parseInt(standardGame.timestamp.N);
-             }
-             // Only count bankroll if it looks like they played
-             if (standardGame.bankroll && standardGame.bankroll.N) {
-               const bankroll = parseInt(standardGame.bankroll.N);
+               entry.standard = {};
+               entry.nonService = true;
+               if (standardGame.hands && standardGame.hands.N) {
+                 entry.standard.hands = parseInt(standardGame.hands.N);
+               }
+               if (standardGame.timestamp && standardGame.timestamp.N) {
+                 entry.standard.timestamp = parseInt(standardGame.timestamp.N);
+               }
+               // Only count bankroll if it looks like they played
+               if (standardGame.bankroll && standardGame.bankroll.N) {
+                 const bankroll = parseInt(standardGame.bankroll.N);
 
-               if (entry.standard.hands || (bankroll !== 5000)) {
-                 entry.standard.bankroll = parseInt(standardGame.bankroll.N);
+                 if (entry.standard.hands || (bankroll !== 5000)) {
+                   entry.standard.bankroll = parseInt(standardGame.bankroll.N);
+                 }
                }
              }
-           }
 
-          if (data.Items[i].mapAttr.M.tournament && data.Items[i].mapAttr.M.tournament.M) {
-            const tournament = data.Items[i].mapAttr.M.tournament.M;
+            if (data.Items[i].mapAttr.M.tournament && data.Items[i].mapAttr.M.tournament.M) {
+              const tournament = data.Items[i].mapAttr.M.tournament.M;
 
-            entry.tournament = {};
-            if (tournament.hands && tournament.hands.N) {
-              entry.tournament.hands = parseInt(tournament.hands.N);
-            }
-            if (tournament.timestamp && tournament.timestamp.N) {
-              entry.tournament.timestamp = parseInt(tournament.timestamp.N);
-            }
-            // Only count bankroll if it looks like they played
-            if (tournament.bankroll && tournament.bankroll.N) {
-              const bankroll = parseInt(tournament.bankroll.N);
+              // Only count tournament scores that are still active
+              if (tournament.timestamp && tournament.timestamp.N &&
+                  (parseInt(tournament.timestamp.N) > tournamentClose)) {
+                entry.tournament = {};
+                if (tournament.hands && tournament.hands.N) {
+                  entry.tournament.hands = parseInt(tournament.hands.N);
+                }
+                entry.tournament.timestamp = parseInt(tournament.timestamp.N);
+                // Only count bankroll if it looks like they played
+                if (tournament.bankroll && tournament.bankroll.N) {
+                  const bankroll = parseInt(tournament.bankroll.N);
 
-              if (entry.hands || (bankroll !== 5000)) {
-                entry.tournament.bankroll = parseInt(tournament.bankroll.N);
+                  if (entry.hands || (bankroll !== 5000)) {
+                    entry.tournament.bankroll = parseInt(tournament.bankroll.N);
+                  }
+                }
               }
             }
-          }
-           results.push(entry);
+             results.push(entry);
+           }
          }
-       }
 
-       if (data.LastEvaluatedKey) {
-         return loop(false, data.LastEvaluatedKey);
-       }
-     });
-   }
-  })(true, null).then(() => {
-    callback(null, results, newads);
-  }).catch((err) => {
-    callback(err, null), null;
+         if (data.LastEvaluatedKey) {
+           return loop(false, data.LastEvaluatedKey);
+         }
+       });
+     }
+    })(true, null).then(() => {
+      callback(null, results, newads);
+    }).catch((err) => {
+      callback(err, null), null;
+    });
   });
 }
 
@@ -284,6 +324,21 @@ function getProgressive(game, callback) {
       }
 
       callback(game, hands, jackpots);
+    }
+  });
+}
+
+function getLastCloseTime(callback) {
+  s3.getObject({Bucket: 'garrett-alexa-usage', Key: 'BlackjackTournamentResults.txt'}, (err, data) => {
+    if (err) {
+      console.log(err, err.stack);
+      callback(0);
+    } else {
+      // Yeah, I can do a binary search (this is sorted), but straight search for now
+      const results = JSON.parse(data.Body.toString('ascii'));
+
+      results.sort((a, b) => (b.timestamp - a.timestamp));
+      callback(results[0].timestamp);
     }
   });
 }
