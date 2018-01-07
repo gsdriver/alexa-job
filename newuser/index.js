c70121d63f8782ae126a97f8987c8122856fbd62
Garrett Vargas <gsdriver@live.com>
2018-01-07 09:59:37 -0800

    New user lambda function

--- newuser/index.js
+++ newuser/index.js
@@ -9,9 +9,18 @@ AWS.config.update({region: 'us-east-1'});
 const doc = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
 const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
-exports.handler = function(event, context) {
+exports.handler = function(event, context, callback) {
+  // Note if this is triggered by an s3 upload
+  if (event.Records && (event.Records.length > 0)) {
+    if (event.Records[0].s3 && event.Records[0].s3.object) {
+      console.log('New users triggered by ' + event.Records[0].s3.object.key);
+    }
+  }
+  context.callbackWaitsForEmptyEventLoop = false;
+
   saveNewUsers(() => {
-    context.succeed();
+    console.log('New users saved!');
+    callback();
   });
 };
 
@@ -46,6 +55,9 @@ function saveNewUsers(callback) {
     if (data && data.Item && data.Item.newUsers) {
       details.slots = parseInt(data.Item.newUsers);
     }
+    if (--numCalls === 0) {
+      completed();
+    }
   });
 
   doc.get({TableName: 'VideoPoker', Key: {userId: 'game'}},
@@ -75,6 +87,7 @@ function saveNewUsers(callback) {
       Key: 'newusers/' + now + '.txt'};
     const Item = {userId: 'game', newUsers: '0'};
 
+    console.log(JSON.stringify(params));
     numCalls = 5;
     s3.putObject(params, (err, data) => {
       // And reset the DBs
