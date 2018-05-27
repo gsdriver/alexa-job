12e676cb67cbb0adf58706085fb9ccdc543d0529
Garrett Vargas <gsdriver@live.com>
2018-05-27 04:48:48 -0700

    Read tournament players

---
+++ tournamentPlayers.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+AWS.config.update({
+  region: 'us-east-1',
+});
+const doc = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+const players = [];
+
+processDBEntries('Slots',
+  (item) => {
+    const attributes = item.mapAttr;
+
+    if (attributes && attributes.tournamentsPlayed) {
+      players.push({user: item.userId, times: attributes.tournamentsPlayed});
+    }
+  },
+  (err, results) => {
+  if (err) {
+    callback('Error processing data: ' + err);
+  } else {
+    console.log(JSON.stringify(players));
+  }
+});
+
+function processDBEntries(dbName, callback, complete) {
+  const results = [];
+
+  // Loop thru to read in all items from the DB
+  (function loop(firstRun, startKey) {
+   const params = {TableName: dbName};
+
+   if (firstRun || startKey) {
+     params.ExclusiveStartKey = startKey;
+
+     const scanPromise = doc.scan(params).promise();
+     return scanPromise.then((data) => {
+       let i;
+
+       for (i = 0; i < data.Items.length; i++) {
+         const entry = callback(data.Items[i]);
+         if (entry) {
+           results.push(entry);
+         }
+       }
+
+       if (data.LastEvaluatedKey) {
+         return loop(false, data.LastEvaluatedKey);
+       }
+     });
+   }
+  })(true, null).then(() => {
+    complete(null, results);
+  }).catch((err) => {
+    console.log(err.stack);
+    complete(err);
+  });
+}
