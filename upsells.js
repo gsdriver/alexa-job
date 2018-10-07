64d04e88b13bcd06a3e8a85480d00ebed3f83c09
Garrett Vargas <gsdriver@live.com>
2018-10-07 04:31:36 -0700

    Track upsell status

---
+++ upsells.js
@@ -0,0 +1,99 @@
+//
+// Utility functions
+//
+
+'use strict';
+
+const fs = require('fs');
+const contentDir = 'content/slots-upsell';
+const csvFile = 'upsell-prompts.csv';
+
+// Read every file from the content directory
+function readFiles(dirname, callback) {
+  const results = [];
+
+  fs.readdir(dirname, (err, filenames) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      let fileCount = filenames.length;
+
+      filenames.forEach((filename) => {
+        fs.readFile(dirname + '/' + filename, 'utf-8', function(err, content) {
+          const timestamp = parseInt(this.filename.replace('.txt', ''));
+          if (err) {
+            callback(err, null);
+          } else {
+            const result = JSON.parse(content);
+            result.timestamp = timestamp;
+            results.push(result);
+            if (--fileCount === 0) {
+              callback(null, results);
+            }
+          }
+        }.bind({filename: filename}));
+      });
+    }
+  });
+}
+
+function getFormattedDate(timestamp) {
+  const date = new Date(timestamp);
+  const year = date.getFullYear();
+  const month = (1 + date.getMonth()).toString();
+  const day = date.getDate().toString();
+  const hour = date.getHours().toString();
+  let minute = date.getMinutes().toString();
+  if (minute.length === 1) {
+    minute = '0' + minute;
+  }
+
+  return month + '/' + day + '/' + year + ' ' + hour + ':' + minute;
+}
+
+// Delete the csv file if it exists
+if (fs.existsSync(csvFile)) {
+  fs.unlinkSync(csvFile);
+}
+
+// Read files and write to a CSV file
+readFiles(contentDir, (err, results) => {
+  if (err) {
+    console.log(err);
+  } else {
+    // Now go through each result and write to a CSV file
+    let text = 'Date,Action,Selection,Response,Token,userId\n';
+
+    results.forEach((result) => {
+      text += getFormattedDate(result.timestamp);
+      text += ',';
+      if (result.action !== undefined) {
+        text += result.action;
+      }
+      text += ',';
+      if (result.selection !== undefined) {
+        text += result.selection;
+      }
+      text += ',';
+      if (result.response !== undefined) {
+        text += result.response;
+      }
+      text += ',';
+      if (result.token !== undefined) {
+        text += result.token;
+      }
+      text += ',';
+      if (result.userId !== undefined) {
+        text += result.userId;
+      }
+      text += '\n';
+    });
+
+    fs.writeFile(csvFile, text, (err) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log('done');
+    });
+  }
+});
